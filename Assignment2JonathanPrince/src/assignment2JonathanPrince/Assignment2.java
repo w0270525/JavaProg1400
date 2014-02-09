66b82a9fef11451b722eb2e0470238865d87f0d1
Jon <Jon@Jonathan>
2014-02-09 19:56:09 -0400

    Assignment 2 progress push

--- Assignment2JonathanPrince/src/assignment2JonathanPrince/Assignment2.java
+++ Assignment2JonathanPrince/src/assignment2JonathanPrince/Assignment2.java
@@ -9,11 +9,16 @@ package assignment2JonathanPrince;
 import javax.swing.JOptionPane;
 
 public class Assignment2 {
+	
+	// Rows and columns for flight booking seating, and array for flight booking.
+	final int ROWS = 4;
+	final int COLUMNS= 4;
+	int[][] flightSeating = new int[ROWS][COLUMNS];
 		
-
-		
+	
 	public void start()
 	{
+		// to get program started.
 		opening();
 	}
 	
@@ -21,7 +26,13 @@ public class Assignment2 {
 	public void opening()
 	{
 		int newBooking = 0;
-		//
+		// Start of booking process with seating arraignment.
+		String s1= "<html><body><table>";
+		
+		
+				
+		String s4= "</table></body></html>";
+		
 		Object[] options = {"Book a New Passenger",
 		                    "Exit"};
 		newBooking = JOptionPane.showOptionDialog(null, "Welcome to the Flight Booker", "Flight Booker",
@@ -36,9 +47,30 @@ public class Assignment2 {
 
 	public void booking()
 	{
-		Object[] options = {"First Class", "Economy"};
-		JOptionPane.showOptionDialog(null, "Would you like First Class or Economy Class?", "Select a Class", JOptionPane.YES_NO_OPTION, options, null, options, initialValue)
+		int chosenClass = 0;
+				
+		Object[] options2 = {"First Class", "Economy"};
+		chosenClass = JOptionPane.showOptionDialog(null, "Would you like First Class or Economy Class?",
+				"Select a Class", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options2, chosenClass);
+		
+				
+				//(null, "Would you like First Class or Economy Class?", "Select a Class", JOptionPane.YES_NO_OPTION, options2, options2, chosenClass);
 	}
 
+	public static String checkSeating(int seat) {
+		// checks if seating is empty returning red or full returning green.
+		if(seat == 1)
+		{
+			return "'green'";
+		}
+		else
+		{
+			return "'red'";
+		}
+	}
+	
+	public static String seatingTable(int seatingArray)  {
+		
+	}
 }
 
